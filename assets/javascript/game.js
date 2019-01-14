cc0afdd131cdbd07ba1f440655eea029423a85b5
Kelly davis <kellyadavis7@gmail.com>
2019-01-14 16:30:55 -0500

    tutor helped

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,68 @@
 $(document).ready(function() {
     
-    // need function that will reset game after user is alerted with result
-    // this function will determine if the user won or lost the game, and then adjust the DOM with their scores
-    // this function can be called in each click function, after the users button value is loaded to the DOM
-
-    // do i need to mention "i" anywhere else?
-    // (i = tries)
-    // for (let i = 0; i < 5; i++){play game}
-    // else {alert congrats, then location.reload}
-
-    
-    // // using "lives"
-    // var lives = 3;
-    // var livesText;
-    // var lifeLostText = "You Lose!"
-
-    // function lose(){
-    //     lives--;
-    //     losses++;
-    //     if(lives){
-    //         $("#losses").text(losses);
-    //         lifeLostText.visible = true;
-    //         if (newSum > randomNum){
-    //             losses++;
-    //             alert("You lose");
-    //             $("#scoreNumber").empty();
-                
-
-    //     }
-    // }
+    //  variables 
+    var randomNum;
+    var crys1val;
+    var crys2val;
+    var crys3val;
+    var crys4val;
+    var scoreArray;
+    var newSum;
+    var wins;
+    var losses;
 
 
+    // resets game vars
+    function game(){
+        randomNum = Math.floor((Math.random() * 120) + 19);
+        crys1val = Math.floor((Math.random() * 12) + 1);
+        crys2val = Math.floor((Math.random() * 12) + 1);
+        crys3val = Math.floor((Math.random() * 12) + 1);
+        crys4val = Math.floor((Math.random() * 12) + 1);
+        scoreArray = [];
+        newSum = 0;
+        $("#scoreNumber").empty();
+    };
 
 
-    var randomNum = Math.floor((Math.random() * 120) + 19);
-    var crys1val = Math.floor((Math.random() * 12) + 1);
-    var crys2val = Math.floor((Math.random() * 12) + 1);
-    var crys3val = Math.floor((Math.random() * 12) + 1);
-    var crys4val = Math.floor((Math.random() * 12) + 1);
-    var scoreArray = [];
+    function checkWins(){
+        if (newSum == randomNum){
+            alert("You win!");
+            wins++;
+            $("#wins").text(wins)
+            game();
+        }
+        else if (newSum > randomNum){
+            alert("you lose!");
+            losses++;
+            $("#losses").text(losses)
+            game();
+        }
+    };
+
+
+
     const getSum = function getSum(total, num) {
         return total + Math.round(num);
     }
-    var wins = 0;
-    var losses = 0;
+
     var sumFunc = function getSum(total, num) {
         return total + num;
     };
 
 
-    
-
-// for (wins = 0; wins < 4; wins++){
+    game();
 
     $("#random").text(randomNum);
 
         $("#crystal1").click(function() {
             scoreArray.push(crys1val);
             sumFunc();
-            var newSum = scoreArray.reduce(getSum)
+            newSum = scoreArray.reduce(getSum)
             $("#scoreNumber").text(newSum)
-
+            console.log("newSum", newSum)
+            console.log("randomNum", randomNum)
+            checkWins();
         });
 
         $("#crystal2").click(function() {
@@ -98,7 +100,5 @@ $(document).ready(function() {
                 }
 
             });
-    // }
-    
 
 });
